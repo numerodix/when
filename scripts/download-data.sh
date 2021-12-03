241e3fa1a691317927de7984bac0dfab8d7757b4
Martin Matusiak <numerodix@gmail.com>
2021-12-04 10:19:45 +1100

    make 'sydney' refer to Aus
    Filter data file to remove this entry:
    
    ```
    location: Sydney (Canada)
    zone: America/Glace_Bay (-0400)
    ```
    
    from matching 'sydney' when we want Sydney, Aus

--- scripts/download-data.sh
+++ scripts/download-data.sh
@@ -4,6 +4,7 @@ if [ ! -f data/cities15000.txt ]; then
   echo "Downloading cities15000"
   curl -L --progress-bar http://download.geonames.org/export/dump/cities15000.zip > data/cities15000.zip
   (cd data; unzip -q cities15000.zip; rm cities15000.zip)
+  sed -i '/Glace/d' data/cities15000.txt
 fi
 if [ ! -f data/countryInfo.txt ]; then
   echo "Downloading CountryInfo"
